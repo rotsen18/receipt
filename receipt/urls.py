b32858b8547cdb5ef4e582e22417d18dea9ecccb
t.nester <t.nester@disoft.dev>
2023-02-25 21:35:31 +0200

    add static url

--- receipt/urls.py
+++ receipt/urls.py
@@ -14,13 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from drf_spectacular.views import SpectacularAPIView, SpectacularJSONAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path(f'api/v1/{settings.ADMIN_URL}', admin.site.urls),
-]
+] + + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.SWAGGER_URL:
     urlpatterns += [
