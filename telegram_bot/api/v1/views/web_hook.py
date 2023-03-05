0e4e91659dde05a02703b28a0024608c1b76d563
t.nester <t.nester@disoft.dev>
2023-03-05 19:59:28 +0200

    try full requirements

--- telegram_bot/api/v1/views/web_hook.py
+++ telegram_bot/api/v1/views/web_hook.py
@@ -1,5 +1,5 @@
 from django.http import JsonResponse
-from telebot.types import Update
+from telegram import Update
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
