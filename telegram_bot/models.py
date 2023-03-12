a40134ed13305eeb382371ccb2f560ee2c40ae7d
t.nester <t.nester@disoft.dev>
2023-03-12 15:14:53 +0200

    give user type admin or client

--- telegram_bot/models.py
+++ telegram_bot/models.py
@@ -3,6 +3,11 @@ from django.db import models
 
 
 class TelegramUser(models.Model):
+    class TypeUserChoice(models.TextChoices):
+        ADMIN = ('Admin', 'Admin')
+        CLIENT = ('Client', 'Client')
+
+    type_user = models.CharField(max_length=15, choices=TypeUserChoice.choices, default=TypeUserChoice.CLIENT)
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL,
         null=True,
@@ -14,3 +19,7 @@ class TelegramUser(models.Model):
     last_name = models.CharField(max_length=35, null=True, default='')
     full_name = models.CharField(max_length=35, null=True, default='')
     username = models.CharField(max_length=35)
+
+    @property
+    def is_telegram_admin(self):
+        return self.type_user == self.TypeUserChoice.ADMIN
