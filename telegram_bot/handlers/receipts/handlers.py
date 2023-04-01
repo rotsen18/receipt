943d8ba202843d53e995c0292107912a75688e31
t.nester <t.nester@disoft.dev>
2023-04-01 19:38:05 +0300

    try to fix receipt url

--- telegram_bot/handlers/receipts/handlers.py
+++ telegram_bot/handlers/receipts/handlers.py
@@ -76,10 +76,8 @@ def edit_receipt(update: Update, context: CallbackContext):
 
 def add_receipt(update: Update, context: CallbackContext):
     create_url = reverse('admin:culinary_receipt_add')
-    base_url = settings.RENDER_EXTERNAL_HOSTNAME or 'https://receipt-5w35.onrender.com'
-    print(base_url)
-    full_url = urljoin(base_url, create_url)
-    update.message.reply_text(text=f'{full_url=}, {base_url}, {create_url}')
+    base_url = settings.RENDER_EXTERNAL_HOSTNAME
+    update.message.reply_text(text=f'{base_url}{create_url}')
 
 
 def view_comments(update: Update, context: CallbackContext):
