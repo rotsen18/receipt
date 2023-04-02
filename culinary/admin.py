a004a0f2abedd6a3357de8f17c1035d89855d98f
t.nester <t.nester@disoft.dev>
2023-04-02 11:44:45 +0300

    set author to current user

--- culinary/admin.py
+++ culinary/admin.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.contrib import admin
 
 from culinary.models import (
@@ -6,12 +7,26 @@ from culinary.models import (
 )
 
 
+class ReceiptForm(forms.ModelForm):
+    class Meta:
+        model = Receipt
+        fields = '__all__'
+        widgets = {'author': forms.HiddenInput()}
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if not self.instance.author:
+            self.fields['author'].initial = self.request.user
+
+
 class ComponentsInline(admin.TabularInline):
     model = ReceiptComponent
+    extra = 1
 
 
 @admin.register(Receipt)
 class ReceiptAdmin(admin.ModelAdmin):
+    form = ReceiptForm
     inlines = [ComponentsInline]
 
 
