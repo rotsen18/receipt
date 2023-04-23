98fbad6be8e8398569dfdbc4268b2fa1efd29ae0
t.nester <t.nester@disoft.dev>
2023-04-23 14:00:51 +0300

    change service to get data from db

--- culinary/services.py
+++ culinary/services.py
@@ -57,7 +57,7 @@ class ReceiptPriceService:
 
     def get_component_price(self, component: ReceiptComponent) -> float:
         ingredient_unit = component.measurement_unit or component.ingredient.default_measurement_unit
-        product_info = self.get_product_info(component.ingredient)
+        product_info = component.ingredient.product_data
         if not product_info:
             return 0
         product_price = product_info['price'] * 0.01
