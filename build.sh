bb855500457461cfeb3410c158b82f1a4a3b767c
t.nester <t.nester@disoft.dev>
2023-06-17 21:04:44 +0300

    remove update product on build

--- build.sh
+++ build.sh
@@ -7,5 +7,4 @@ pip3 install -r requirements.txt
 
 python3 manage.py collectstatic --no-input
 python3 manage.py migrate
-python3 manage.py compilemessages
-python3 manage.py update_products
+python3 manage.py compilemessages ,
