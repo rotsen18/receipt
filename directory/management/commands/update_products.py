dc1d2bf60ad73d2e609580f48d2f161ab32685dc
Taras Nester <102423887+rotsen18@users.noreply.github.com>
2023-06-18 21:57:35 +0300

    async product update service (#64)
    * update new packages
    
    * create new async service to update product from store
    
    * change command to use new service

--- directory/management/commands/update_products.py
+++ directory/management/commands/update_products.py
@@ -1,23 +1,10 @@
 from django.core.management.base import BaseCommand
 
-from culinary.services import ReceiptPriceService
-from directory.models import Ingredient
+from culinary.services import UpgradeProduct
 
 
 class Command(BaseCommand):
-    help = 'Update products data'
 
     def handle(self, *args, **options):
-        bulk_ingredients = []
-        for ingredient in Ingredient.objects.all():
-            try:
-                data = ReceiptPriceService.get_product_info(ingredient)
-            except Exception as e:
-                self.stdout.write(self.style.ERROR(f'{ingredient}: status Error: {e}'))
-                continue
-            self.stdout.write(self.style.SUCCESS(f'{ingredient}: status OK'))
-            ingredient.product_data = data
-            bulk_ingredients.append(ingredient)
-        Ingredient.objects.bulk_update(bulk_ingredients, ['product_data'])
-        self.stdout.write(self.style.SUCCESS('All data updated successfully'))
+        UpgradeProduct.update_all_products_data()
         return
